60415a5c1136b34ad20cbaf05c433108d09bd5a1
ChaRam05 <charmaineramirez05@gmail.com>
2024-05-20 16:41:12 +0800

    Removed useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { ForumProvider } from './contexts/ForumContext'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-
-import HomePage from './pages/HomePage'
-import ForumsPage from './pages/ForumsPage'
-import MainLayout from './layouts/MainLayout'
-import CreateNewForum from './pages/CreateNewForum'
-import ForumPage from './pages/ForumPage'
-
-
+import HomePage from "./pages/HomePage";
+import ForumsPage from "./pages/ForumsPage";
+import MainLayout from "./layouts/MainLayout";
+import CreateNewForum from "./pages/CreateNewForum";
+import ForumPage from "./pages/ForumPage";
 
 const App = () => {
-
-
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path='/:topic' element={<ForumsPage />} />
-        <Route path='/forum/:id' element={<ForumPage />}  />
-      
+        <Route path="/:topic" element={<ForumsPage />} />
+        <Route path="/forum/:id" element={<ForumPage />} />
 
-        <Route path='/createNewForum' element={<CreateNewForum />} />
+        <Route path="/createNewForum" element={<CreateNewForum />} />
       </Route>
     )
   );
 
+  return <RouterProvider router={router} />;
+};
 
-
-
-  return (
-    <ForumProvider>
-      <RouterProvider router={router} />
-    </ForumProvider>
-  );
-}
-
-export default App
+export default App;
