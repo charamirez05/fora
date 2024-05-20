21731377acc7c823824e437aa5b5240a07f23bb9
ChaRam05 <charmaineramirez05@gmail.com>
2024-05-20 10:45:27 +0800

    Added MUI pagination to viewing all forums

--- src/components/ForumsListing.jsx
+++ src/components/ForumsListing.jsx
@@ -3,16 +3,50 @@ import ForumCard from "./ForumCard";
 import { Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Button, Stack, Tab, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Pagination,
+  Stack,
+  Tab,
+  TablePagination,
+  Typography,
+} from "@mui/material";
 import { useForum } from "../contexts/ForumContext";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const ForumsListing = ({ forums, topic }) => {
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 4;
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedForums = forums && forums.slice(startIndex, endIndex);
 
   return (
     <Box
       sx={{ backgroundColor: "rgba(209, 250, 229, 0.3)", paddingTop: "100px" }}
     >
+      <Box style={{ display: "flex", paddingLeft: "30px" }}>
+        <Link
+          to={"/"}
+          className="text-indigo-500 hover:text-indigo-600 flex items-center"
+        >
+          <ArrowBackIcon className="mr-2" style={{ color: "#E2725B" }} />
+          <Typography
+            variant="h6"
+            style={{
+              color: "#3F826D",
+              fontWeight: "bold",
+            }}
+          >
+            {topic} Forums
+          </Typography>
+        </Link>
+      </Box>
       <Box
         sx={{
           backgroundColor: "white", // bg-white
@@ -22,28 +56,25 @@ const ForumsListing = ({ forums, topic }) => {
           borderRadius: "10px", // rounded-md (8px border radius)
         }}
       >
-        <Box style={{ display: "flex" }}>
-          <Link
-            to={"/"}
-            className="text-indigo-500 hover:text-indigo-600 flex items-center"
-          >
-            <ArrowBackIcon className="mr-2" style={{ color: "#E2725B" }} />
-            <Typography
-              variant="h6"
-              style={{
-                color: "#3F826D",
-                fontWeight: "bold",
-              }}
-            >
-              {topic} Forums
-            </Typography>
-          </Link>
-        </Box>
-
         <Box sx={{ margin: "10px" }}>
-          {forums && forums.map((forum) => (
-            <ForumCard key={forum.id} forum={forum} topic={topic}/>
-          ))}
+          {paginatedForums &&
+            paginatedForums.map((forum) => (
+              <ForumCard key={forum.id} forum={forum} topic={topic} />
+            ))}
+          <Box display="flex" justifyContent="center">
+            <Pagination
+              count={Math.ceil(forums && forums.length / itemsPerPage)}
+              page={page}
+              onChange={handleChange}
+              sx={{
+                "& .Mui-selected": {
+                  color: "white",
+                  backgroundColor: "#E2725B",
+                },
+                
+              }}
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
